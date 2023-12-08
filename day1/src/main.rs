7d9bc9b680139184e05c297cef6a6630d2760fdd
Tolga Ayvazoglu <tolga.ayvazoglu@devoteam.com>
2023-12-08 21:42:44 +0100

    push before rethink

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -8,63 +8,85 @@ fn find_digits(line: &String) -> (u32, u32) {
     let mut number_in_words = "".to_string();
 
     for (idx, _) in line.chars().enumerate() {
-        for (_, char) in line.chars().enumerate().skip(idx).take(idx + 5) {
+        println!();
+        //"abcone2threexyz"
+        // TODO: take fn have wrong params
+        for (_, char) in line.chars().enumerate().skip(idx).take(5) {
+            println!("{:?}", line.chars().enumerate().skip(idx).take(5).collect::<Vec<_>>());
             if char.is_numeric() {
                 if first == 0 {
                     first = char.to_digit(10).unwrap();
+                    println!();
+                    println!("added! (first): {}", first);
+                    break;
                 }
                 last = char.to_digit(10).unwrap();
+                println!();
+                println!("added! (last): {}", last);
+                break;
+
             } else {
                 number_in_words.push(char);
-
-                if number_in_words.len() >= 3 && number_in_words.eq("one")
-                    || number_in_words.eq("two")
-                    || number_in_words.eq("three")
-                    || number_in_words.eq("four")
-                    || number_in_words.eq("five")
-                    || number_in_words.eq("six")
-                    || number_in_words.eq("seven")
-                    || number_in_words.eq("eight")
-                    || number_in_words.eq("nine")
-                {
+                if is_word_a_number(&number_in_words) {
                     if first == 0 {
                         first = map_word_to_number(&number_in_words)
                             .expect("i expect a number word from 1 - 9");
 
+                        println!();
+                        println!("added! (first): {}", first);
                         number_in_words = "".to_string();
                         break;
                     }
                     last = map_word_to_number(&number_in_words)
                         .expect("i expect a number word from 1 - 9");
 
+                    println!();
+                    println!("added! (last): {}", last);
                     number_in_words = "".to_string();
                     break;
-                } else if number_in_words.len() == 5 {
+                }
+
+                if number_in_words.len() == 5 {
                     number_in_words = "".to_string();
+                    break;
                 }
             }
         }
     }
 
-    println!("first: {}, last: {}", first, last);
-
     return (first, last);
 }
 
+fn is_word_a_number(number_in_words: &String) -> bool {
+    if number_in_words.len() >= 3 && number_in_words.contains("one")
+        || number_in_words.contains("two")
+        || number_in_words.contains("three")
+        || number_in_words.contains("four")
+        || number_in_words.contains("five")
+        || number_in_words.contains("six")
+        || number_in_words.contains("seven")
+        || number_in_words.contains("eight")
+        || number_in_words.contains("nine")
+    {
+        return true;
+    }
+
+    return false;
+}
+
 fn map_word_to_number(string: &str) -> Option<u32> {
-    println!("current word: {}", string);
-    Some(match string {
-        "one" => 1,
-        "two" => 2,
-        "three" => 3,
-        "four" => 4,
-        "five" => 5,
-        "six" => 6,
-        "seven" => 7,
-        "eight" => 8,
-        "nine" => 9,
-        _ => return None, // Handle the case where the input is not a valid word
-    })
+    match string {
+        s if s.contains("one") => Some(1),
+        s if s.contains("two") => Some(2),
+        s if s.contains("three") => Some(3),
+        s if s.contains("four") => Some(4),
+        s if s.contains("five") => Some(5),
+        s if s.contains("six") => Some(6),
+        s if s.contains("seven") => Some(7),
+        s if s.contains("eight") => Some(8),
+        s if s.contains("nine") => Some(9),
+        _ => None,
+    }
 }
 
 fn concat_numbers(first: u32, last: u32) -> u32 {
