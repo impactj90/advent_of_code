b90325bd86528cb7d070881242e3b557ff36b315
Tolga Ayvazoglu <tolga.ayvazoglu@devoteam.com>
2023-12-24 17:26:12 +0100

    remove bins

--- day2/target/debug/day2.d
+++
@@ -1,1 +0,0 @@
-/home/theimpact/projects/advent_of_code/day2/target/debug/day2: /home/theimpact/projects/advent_of_code/day2/src/main.rs
--- day2/target/debug/deps/day2-0a8066942817fe57.d
+++
@@ -1,5 +0,0 @@
-/home/theimpact/projects/advent_of_code/day2/target/debug/deps/libday2-0a8066942817fe57.rmeta: src/main.rs
-
-/home/theimpact/projects/advent_of_code/day2/target/debug/deps/day2-0a8066942817fe57.d: src/main.rs
-
-src/main.rs:
